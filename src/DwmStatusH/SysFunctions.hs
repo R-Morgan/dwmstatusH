e392c7a022ba289dd9acd5ccc6c3bceff0a5787d
R-Morgan <rmorgan@trystero.is>
2014-10-30 10:32:06 -0700

    Restructure function to return IO Strings
    These functions are intended for use in a do-block in the Main.hs
    function. Hopefully the results can be bound to names and used
    to form a string to be pushed up to WM_NAME in the root X window.

--- src/DwmStatusH/SysFunctions.hs
+++ src/DwmStatusH/SysFunctions.hs
@@ -10,10 +10,17 @@ import Data.Time.LocalTime
 import System.Locale
 import System.Posix.LoadAvg
 
+snatchTime :: IO String
 snatchTime = do ct <- getClockTime
-                print ct
+                let timeStr = show ct
+                return timeStr 
 
+snatchLoadAvg :: IO String
 snatchLoadAvg = do ldavg <- getLoadAvg
                    let avg1 = sample_1 ldavg
-                   print $ show  avg1
+                       avg5 = sample_5 ldavg
+                       avg15 = sample_15 ldavg
+                       loadList = avg1:avg5:[avg15]
+                       loadStrings = unwords $ map show loadList 
+                   return loadStrings
 
