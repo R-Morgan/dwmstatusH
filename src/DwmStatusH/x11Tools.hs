99f26b0520d329532a93796181f091fd72265fdc
R-Morgan <rmorgan@trystero.is>
2014-10-30 09:25:09 -0700

    Merge x11-based tools into one file

---
+++ src/DwmStatusH/x11Tools.hs
@@ -0,0 +1,34 @@
+module Main where
+import Graphics.X11.Xlib
+import Graphics.X11.Xlib.Extras (getTextProperty, wcTextPropertyToTextList)
+import System.Exit (exitWith, ExitCode(..))
+import Control.Concurrent (threadDelay)
+
+main :: IO ()
+main = do
+ dpy <- openDisplay ""
+ let dflt = defaultScreen dpy
+     border = blackPixel dpy dflt
+     background = whitePixel dpy dflt
+ rootw <- rootWindow dpy dflt
+ wmText <- getTextProperty dpy rootw wM_NAME
+ wmString <-  wcTextPropertyToTextList dpy wmText
+ sync dpy False 
+ putStrLn $ wmString !! 0
+ exitWith ExitSuccess
+module Main where
+import Graphics.X11.Xlib
+import System.Exit (exitWith, ExitCode(..))
+import Control.Concurrent (threadDelay)
+
+main :: IO ()
+main = do
+ dpy <- openDisplay ""
+ let dflt = defaultScreen dpy
+     border = blackPixel dpy dflt
+     background = whitePixel dpy dflt
+ rootw <- rootWindow dpy dflt
+ setTextProperty dpy rootw "Hiya Wormy, I'm Aria Goodhalo!" wM_NAME
+ sync dpy False 
+ threadDelay(10 * 1000000)
+ exitWith ExitSuccess
